ba93b1478272904bc5ed06a9a169f904f9fa4a04
EDUARDO <eiglesias@platkmframework.org>
2023-08-29 17:28:19 +0200

    init commit

---
+++ src/main/java/org/platkmframework/comon/service/exception/ServiceException.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ *   Copyright(c) 2023 the original author or authors.
+ *  
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *  
+ *        https://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *******************************************************************************/
+package org.platkmframework.comon.service.exception;
+
+import org.platkmframework.annotation.TruslyException; 
+
+
+/**
+ *   Author: 
+ *     Eduardo Iglesias
+ *   Contributors: 
+ *   	Eduardo Iglesias - initial API and implementation
+ **/
+@TruslyException
+public class ServiceException extends Exception implements StatusException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public int status;
+	
+	public ServiceException(int status, String arg0) {
+		super(arg0); 
+		this.status = status; 
+	}
+	
+	public ServiceException(String arg0) {
+		super(arg0);  
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+
+}
